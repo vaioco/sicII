bb53ddc46b8ebd89202855a2f6afcacc4d27c431
vaioco <valerio.costamagna@di.unito.it>
2015-12-13 13:57:35 -0800

    modificato con strcpy

--- esercizi/stack/stack2.c
+++ esercizi/stack/stack2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char** argv) {
 	int cookie;
 	char buf[80];
 
 	printf("buf: %08x cookie: %08x\n", &buf, &cookie);
 	gets(buf);
-
 	if (cookie == 0x01020305)
   		printf("you win!\n");
 }
